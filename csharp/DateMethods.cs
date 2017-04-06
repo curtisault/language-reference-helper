1159a9509764a61e740365dfec96330c706fea1b
Curtis Ault <curtisault@gmail.com>
2017-04-06 16:14:24 -0500

    Update DateMethods.cs

--- csharp/DateMethods.cs
+++ csharp/DateMethods.cs
@@ -1,4 +1,4 @@
 public DateTime GetFirstOfMonth() {
   	var now = DateTime.Now;
-		return var firstOfMonth = new DateTime(now.Year, now.Month, 1, 0, 0, 0);
+	return var firstOfMonth = new DateTime(now.Year, now.Month, 1, 0, 0, 0);
 }
